e73133bebd375ad0cc0c84453f6b3301c55c95d2
Hugodof <hugo.2021120000@unicap.br>
2024-11-21 20:23:57 -0300

    Pág avaliacao concluída

--- ecoff/src/app/avaliacao/avaliacao.component.ts
+++ ecoff/src/app/avaliacao/avaliacao.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-avaliacao',
   templateUrl: './avaliacao.component.html',
-  styleUrl: './avaliacao.component.css'
+  styleUrls: ['./avaliacao.component.css']
 })
 export class AvaliacaoComponent {
+  rating: number = 0; // Classificação atual
+  hoveredRating: number = 0; // Classificação temporária ao passar o mouse
 
+  // Define a avaliação final
+  setRating(rating: number): void {
+    this.rating = rating;
+  }
+
+  // Atualiza a avaliação temporária ao passar o mouse
+  hover(rating: number): void {
+    this.hoveredRating = rating;
+  }
+
+  // Reseta a avaliação temporária ao sair do mouse
+  reset(): void {
+    this.hoveredRating = 0;
+  }
 }
--- ecoff/src/app/menu/menu.component.ts
+++ ecoff/src/app/menu/menu.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
+
 export class MenuComponent {
 
 }
