468c7858d5e4900bce2279bb7c66cb8fba32faed
Hugodof <hugo.2021120000@unicap.br>
2024-11-21 15:52:32 -0300

    Correção de Hierarquia de CSS

--- ecoff/src/app/app-routing.module.ts
+++ ecoff/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component'; // Importa o MenuComponent
+import { ContatoComponent } from './contato/contato.component';
+import { AvaliacaoComponent } from './avaliacao/avaliacao.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MenuComponent }, // Rota padrão redirecionando para Menu
+  { path: 'contato', component: ContatoComponent },
+  { path: 'avaliacao', component: AvaliacaoComponent },
+  { path: 'carrinho', component: CarrinhoComponent },
+  { path: '**', redirectTo: '' }, // Rota wildcard redireciona para a página padrão
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ecoff/src/app/app.module.ts
+++ ecoff/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { AvaliacaoComponent } from './avaliacao/avaliacao.component';
+import { ContatoComponent } from './contato/contato.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CarrinhoComponent,
+    AvaliacaoComponent,
+    ContatoComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
